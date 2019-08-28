6e0b66b6a829ceca019a4e20bd6e45a75b482ba4
flybetter <flybetter@163.com>
2019-08-28 16:58:15 +0800

    Initial commit

---
+++ src/main/java/com/house365/kafkaproducter/KafkaProducterApplication.java
@@ -0,0 +1,17 @@
+package com.house365.kafkaproducter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KafkaProducterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaProducterApplication.class, args);
+    }
+
+
+
+
+
+}
