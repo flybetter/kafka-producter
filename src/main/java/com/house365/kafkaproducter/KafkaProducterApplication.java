f88e51a602665626b21b60905434eb8179cc2067
flybetter <flybetter@163.com>
2019-08-30 10:50:18 +0800

    the demo seem work.

--- src/main/java/com/house365/kafkaproducter/KafkaProducterApplication.java
+++ src/main/java/com/house365/kafkaproducter/KafkaProducterApplication.java
@@ -1,6 +1,8 @@
 package com.house365.kafkaproducter;
 
+import org.apache.kafka.clients.admin.AdminClientConfig;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.IntegerDeserializer;
 import org.apache.kafka.common.serialization.IntegerSerializer;
@@ -8,18 +10,21 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
-import org.springframework.kafka.core.DefaultKafkaProducerFactory;
-import org.springframework.kafka.core.KafkaTemplate;
-import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.core.*;
 import org.springframework.kafka.listener.ContainerProperties;
 import org.springframework.kafka.listener.KafkaMessageListenerContainer;
+import org.springframework.util.StringUtils;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
 
 @SpringBootApplication
@@ -31,6 +36,26 @@ public class KafkaProducterApplication {
     public static void main(String[] args) {
         SpringApplication.run(KafkaProducterApplication.class, args);
     }
+
+//    @Autowired
+//    private KafkaTemplate<String, String> template;
+//
+//    private final CountDownLatch latch = new CountDownLatch(3);
+//
+//    @Override
+//    public void run(String... args) throws Exception {
+//        this.template.send("myTopic", "foo1");
+//        this.template.send("myTopic", "foo2");
+//        this.template.send("myTopic", "foo3");
+//        latch.await(60, TimeUnit.SECONDS);
+//        logger.info("All received");
+//    }
+//
+//    @KafkaListener(topics = "myTopic")
+//    public void listen(ConsumerRecord<?, ?> cr) throws Exception {
+//        logger.info(cr.toString());
+//        latch.countDown();
+//    }
 
 
 }
